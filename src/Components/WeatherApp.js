64677e733af2b7b8ebb266dcbd4479f2dae4001f
JugalSoni <sonijugal2002@gmail.com>
2023-08-21 13:37:57 +0530

    last commit

--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -1,48 +1,85 @@
-import React, { useState } from 'react';
-import './App.css';
-import WeatherInfo from './WeatherInfo';
+import React, { useState } from "react";
+import "./App.css";
+import WeatherInfo from "./WeatherInfo";
 
-const apiKey = '563020b92ab040a197261521232108'; // Replace with your API key from weatherapi.com
+const apiKey = "563020b92ab040a197261521232108"; // Replace with your API key from weatherapi.com
 
 function WeatherApp() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState("");
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
 
   const handleSearch = () => {
-    if (city.trim() === '') {
-      alert('Please enter a city name');
-      return;
+    setIsSubmit(true);
+    const errors = validate(city);
+    setFormErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      fetch(
+        `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setIsSubmit(false);
+          setFormErrors({});
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
+          setWeatherData(null);
+        });
     }
-
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
-      .then(response => response.json())
-      .then(data => setWeatherData(data))
-      .catch(error => {
-        console.error('Error fetching weather data:', error);
-        setWeatherData(null);
-      });
   };
+  const [err, setErr] = useState(false);
+
+  const validate = (city) => {
+    const errors = {};
+
+    if (city.trim() === "") {
+      errors.CityName = "City Name is required";
+      setErr(true);
+    } else {
+      setErr(false);
+    }
+    return errors;
+  };
+
+  const validClass =
+    err && isSubmit ? "form-control is-invalid" : "form-control";
 
   return (
     <div>
-    <h1>Weather App</h1>
-    <div className="weather-container">
-      
-      <input
-        type="text"
-        value={city}
-        onChange={e => setCity(e.target.value)}
-        placeholder="Enter city name"
-      />
-      <button onClick={handleSearch}>Search</button>
-      
-      {weatherData!== null && (
-        <WeatherInfo weatherData={weatherData}/>
-      )}
-      {weatherData===null && (
-        <p>No weather data available</p>
-      )}
-    </div>
+      <h1 className="mb-5 mt-5">Weather App</h1>
+      <div className="weather-container">
+        {/* <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city name"
+        /> */}
+        <div className="row">
+        <div className="form-floating mb-3 col-10  ">
+          <input
+            type="text"
+            className={validClass}
+            placeholder="City Name"
+            id="rolefloatingInput"
+            required
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          {isSubmit && <p className="text-danger">{formErrors.CityName}</p>}
+
+          <label htmlFor="role-field" className="form-label">  Type City Name Here...</label>
+        </div>
+        <div className="col-2" style={{display:"flex", flexDirection:"column", justifyContent:"start", alignItems:"center",verticalAlign:"middle"}}>
+        <button className="btn btn-primary" onClick={handleSearch}>Search</button>
+        </div>
+        </div>
+
+        {weatherData !== null && <WeatherInfo weatherData={weatherData} />}
+        {weatherData === null && <pre style={{margin:"auto",width:"45%", marginTop:"70px"}}>       No Weather Data Available</pre>} 
+      </div>
     </div>
   );
 }
--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -1,27 +1,30 @@
 import React, { useState } from "react";
+import "./App.css"
 
 const WeatherInfo = ({ weatherData }) => {
   const [cel, setCel] = useState(false);
   return (
     <div className="weather-info">
-      <h2>{weatherData.location.name}</h2>
+      <h2>City Name: {weatherData.location.name}</h2>
       <div>
+      <div style={{display:"flex", flexDirection:"row", justifyContent:"center", alignItems:"center", gap:"5"}}>
         <img
           src={weatherData.current.condition.icon}
           alt={weatherData.current.condition.text}
         />
         {cel ? (
-          <p>{weatherData.current.temp_c}°C</p>
+          <p style={{marginLeft:"10px",marginRight:"10px",fontWeight:"bold"}}>Temperature: {weatherData.current.temp_c}°C</p>
         ) : (
-          <p>{weatherData.current.temp_f} F</p>
+          <p style={{marginLeft:"10px",marginRight:"10px",fontWeight:"bold"}}>Temperature: {weatherData.current.temp_f} F</p>
         )}
-        <button onClick={(e)=>{ e.preventDefault(); setCel(true)}} style={{display: cel ? "none" : ""}} >
+        <button  className="btn btn-secondary"  onClick={(e)=>{ e.preventDefault(); setCel(true)}} style={{display: cel ? "none" : ""}} >
         In Celsius
         </button>
-        <button onClick={(e)=>{ e.preventDefault(); setCel(false)}} style={{display: cel ? "" : "none"}} >
+        <button  className="btn btn-secondary" onClick={(e)=>{ e.preventDefault(); setCel(false)}} style={{display: cel ? "" : "none"}} >
         In Fahrenheit
         </button>
-        <p>{weatherData.current.condition.text}</p>
+        </div>
+        <p style={{fontWeight:"bold"}}>Current Weather Condition: {weatherData.current.condition.text}</p>
       </div>
     </div>
   );
